453bb39bc5f51a4617f54ac5c48363d9e6efd7a8
FelipeDania <felipedania@gmail.com>
2023-03-08 13:29:49 -0300

    feat: add backend provider

--- provider.tf
+++ provider.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.57.1"
     }
+  }
+
+  backend "s3" {
+    bucket = "bucket-tf-comunidadedevops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
