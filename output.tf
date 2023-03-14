3cd3995f0f65984813a126fdcf7bc74c47c58d24
FelipeDania <felipedania@gmail.com>
2023-03-14 17:17:09 -0300

    feat: add new cluster module

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "subnet_public_1a" {
   value = module.eks_network.subnet_public_1a
 }
+
+output "eks_vpc_config_out" {
+  value = module.eks_cluster.eks_vpc_config
+}
+
+output "eks_cluster_certificate_identity" {
+  value = module.eks_cluster.tls_certificate_identity
+}
